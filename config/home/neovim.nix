aa7d2a3c194908bfa2e183d735498601cc7d0a0a
Simayon Thampi <simayonthampi@gmail.com>
2024-03-17 00:22:53 +0530

    fix: Neovim Whitespace menace fix
    [Brief]:
    |- Added autocmd support to fix the whitespcae characters
       issue in Neovim

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -8,7 +8,7 @@ in {
     enable = true;
 
     globals.mapleader = " "; # Sets the leader key to space
-    
+
     options = {
       clipboard="unnamedplus";
       number = true;         # Show line numbers
@@ -45,17 +45,17 @@ in {
       base0E = "#${theme.base0E}";
       base0F = "#${theme.base0F}";
     };
-    
+
     plugins = {
       barbecue.enable = true;
       gitsigns.enable = true;
       noice.enable = true;
       telescope = {
-	enable = true;
-	keymaps = {
-	  "<leader>ff" = "find_files";
-	  "<leader>lg" = "live_grep";
-	};
+        enable = true;
+        keymaps = {
+          "<leader>ff" = "find_files";
+          "<leader>lg" = "live_grep";
+        };
       };
       indent-blankline.enable = true;
       nvim-colorizer.enable = true;
@@ -65,125 +65,125 @@ in {
       lualine = {
         enable = true;
       };
-      startup = { 
-	enable = true;
-	theme = "dashboard";
+      startup = {
+        enable = true;
+        theme = "dashboard";
       };
       lsp = {
-	enable = true;
-	servers = {
-	  tsserver.enable = true;
-	  lua-ls.enable = true;
-	  bashls.enable = true;
-	  rust-analyzer = {
-	    enable = true;
-	    installRustc = true;
-	    installCargo = true;
-	  };
-	  nixd.enable = true;
-	  html.enable = true;
-	  ccls.enable = true;
-	  cmake.enable = true;
-	  csharp-ls.enable = true;
-	  cssls.enable = true;
-	  gopls.enable = true;
-	  jsonls.enable = true;
-	  pyright.enable = true;
-	  tailwindcss.enable = true;
-	};
+        enable = true;
+        servers = {
+          tsserver.enable = true;
+          lua-ls.enable = true;
+          bashls.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installRustc = true;
+            installCargo = true;
+          };
+          nixd.enable = true;
+          html.enable = true;
+          ccls.enable = true;
+          cmake.enable = true;
+          csharp-ls.enable = true;
+          cssls.enable = true;
+          gopls.enable = true;
+          jsonls.enable = true;
+          pyright.enable = true;
+          tailwindcss.enable = true;
+        };
       };
       lsp-lines.enable = true;
       treesitter = {
-	enable = true;
-	nixGrammars = true;
+        enable = true;
+        nixGrammars = true;
       };
       nvim-cmp = {
-	enable = true;
-	autoEnableSources = true;
-	sources = [
-	  { name = "nvim_lsp"; }
-	  { name = "path"; }
-	  { name = "buffer"; }
-	];
-	mapping = {
-	  "<CR>" = "cmp.mapping.confirm({ select = true })";
-	  "<Tab>" = {
-	    action = ''cmp.mapping.select_next_item()'';
-	    modes = [ "i" "s" ];
-	  };
-	};
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "buffer"; }
+        ];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            action = ''cmp.mapping.select_next_item()'';
+            modes = [ "i" "s" ];
+          };
+        };
       };
     };
 
     extraPlugins = [ plugins.telescope-file-browser-nvim ];
 
     # FOR NEOVIDE
-    extraConfigLua = '' 
-      vim.opt.guifont = "JetBrainsMono\\ NFM,Noto_Color_Emoji:h14"
-      vim.g.neovide_cursor_animation_length = 0.05
+    extraConfigLua = ''
+    vim.opt.guifont = "JetBrainsMono\\ NFM,Noto_Color_Emoji:h14"
+    vim.g.neovide_cursor_animation_length = 0.05
 
-      local colors = {
-        blue   = '#${theme.base0D}',
-        cyan   = '#${theme.base0C}',
-        black  = '#${theme.base00}',
-        white  = '#${theme.base05}',
-        red    = '#${theme.base08}',
-        violet = '#${theme.base0E}',
-        grey   = '#${theme.base02}',
-      }
+    local colors = {
+      blue   = '#${theme.base0D}',
+      cyan   = '#${theme.base0C}',
+      black  = '#${theme.base00}',
+      white  = '#${theme.base05}',
+      red    = '#${theme.base08}',
+      violet = '#${theme.base0E}',
+      grey   = '#${theme.base02}',
+    }
 
-      local bubbles_theme = {
-        normal = {
-          a = { fg = colors.black, bg = colors.violet },
-          b = { fg = colors.white, bg = colors.grey },
-          c = { fg = colors.black, bg = colors.black },
-        },
+    local bubbles_theme = {
+      normal = {
+        a = { fg = colors.black, bg = colors.violet },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.black, bg = colors.black },
+      },
 
-        insert = { a = { fg = colors.black, bg = colors.blue } },
-        visual = { a = { fg = colors.black, bg = colors.cyan } },
-        replace = { a = { fg = colors.black, bg = colors.red } },
+      insert = { a = { fg = colors.black, bg = colors.blue } },
+      visual = { a = { fg = colors.black, bg = colors.cyan } },
+      replace = { a = { fg = colors.black, bg = colors.red } },
 
-        inactive = {
-          a = { fg = colors.white, bg = colors.black },
-          b = { fg = colors.white, bg = colors.black },
-          c = { fg = colors.black, bg = colors.black },
-        },
-      }
+      inactive = {
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
+        c = { fg = colors.black, bg = colors.black },
+      },
+    }
 
-      require('lualine').setup {
-        options = {
-          theme = bubbles_theme,
-          component_separators = '|',
-          section_separators = { left = '', right = '' },
+    require('lualine').setup {
+      options = {
+        theme = bubbles_theme,
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = {
+          { 'mode', separator = { left = '' }, right_padding = 2 },
         },
-        sections = {
-          lualine_a = {
-            { 'mode', separator = { left = '' }, right_padding = 2 },
-          },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = { 'fileformat' },
-          lualine_x = {},
-          lualine_y = { 'filetype', 'progress' },
-          lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
-          },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = { 'fileformat' },
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+          { 'location', separator = { right = '' }, left_padding = 2 },
         },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-      }
+      },
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      tabline = {},
+      extensions = {},
+    }
     '';
 
     extraConfigVim = ''
-      set noshowmode
-      inoremap jj <ESC>
+    set noshowmode
+    inoremap jj <ESC>
     '';
 
     keymaps = [
@@ -204,7 +204,7 @@ in {
         options.silent = false;
       }
       {
-        key = "<Leader>y"; 
+        key = "<Leader>y";
         action = "\"+y";  #yank into PRIMARY register
         options.silent = true;
       }
@@ -222,6 +222,19 @@ in {
       }
     ];
 
-
+    highlight.ExtraWhitespace.bg = "red";
+    match.ExtraWhitespace = "\\s\\+$";
+    autoCmd = [
+      {
+        event = "FileType";
+        pattern = "nix";
+        command = "setlocal tabstop=2 shiftwidth=2";
+      }
+      {
+        event = "BufWritePre";  # The event at which the cmd is triggered
+        pattern = "*"; # the pattern. Eg nix for only .nix files
+        command = ":%s/\\s\\+$//e"; # command to run on event
+      }
+    ];
   };
- } 
+}
