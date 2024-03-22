954ace313ef0f7b4b0d69afa19bdfbbf29638a9e
Simayon Thampi <simayonthampi@gmail.com>
2024-03-23 00:13:25 +0530

    feat: Snippet & Autocompletion Support Neovim
    [Brief]:
    |- Added snippet and autocompletion support on NeoVim
    
    [Notes]:

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 let
-  plugins = pkgs.vimPlugins;
   theme = config.colorScheme.palette;
 in {
   programs.nixvim = {
@@ -50,6 +49,16 @@ in {
       barbecue.enable = true;
       gitsigns.enable = true;
       noice.enable = true;
+
+      # Snippets
+      luasnip.enable = true;
+      friendly-snippets.enable = true;
+
+      # autocompletion
+      cmp_luasnip.enable = true; #Snippets enable
+      cmp-nvim-lsp.enable = true; #LSP Server for neovim
+      cmp-nvim-lua.enable = true;
+
       telescope = {
         enable = true;
         keymaps = {
@@ -115,7 +124,10 @@ in {
       };
     };
 
-    extraPlugins = [ plugins.telescope-file-browser-nvim ];
+    extraPlugins = with pkgs.vimPlugins; [
+      telescope-file-browser-nvim
+      nvim-lspconfig
+    ];
 
     # FOR NEOVIDE
     extraConfigLua = ''
@@ -179,6 +191,50 @@ in {
       tabline = {},
       extensions = {},
     }
+
+    -- import nvim-cmp plugin safely
+    local cmp_status, cmp = pcall(require, "cmp")
+    if not cmp_status then
+    return
+    end
+
+    -- import luasnip plugin safely
+    local luasnip_status, luasnip = pcall(require, "luasnip")
+    if not luasnip_status then
+    return
+    end
+
+    -- load VSCode-like snippets from plugins (e.g., friendly-snippets)
+
+    require("luasnip/loaders/from_vscode").lazy_load()
+
+    vim.opt.completeopt = "menu,menuone,noselect"
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+        luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
+
+      -- sources for autocompletion
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" }, -- LSP
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" }, -- text within the current buffer
+        { name = "path" }, -- file system paths
+      }),
+    })
+
     '';
 
     extraConfigVim = ''
